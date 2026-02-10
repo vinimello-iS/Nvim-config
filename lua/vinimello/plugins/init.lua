50e15085da7331fd5e5e7d9d9b4bfdbac3cd8541
Vinicius Mello <vinicius.mello@isystems.ai>
2026-02-10 17:53:43 -0300

    update lsp error updates

--- lua/vinimello/plugins/init.lua
+++ lua/vinimello/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
 	'tpope/vim-commentary',
 	'tpope/vim-surround',
-	'tpope/vim-fugitive',
 	'bronson/vim-visual-star-search',
 	'jbgutierrez/vim-better-comments',
 }
--- lua/vinimello/plugins/lsp.lua
+++ lua/vinimello/plugins/lsp.lua
@@ -27,5 +27,6 @@ return {
 	init = function()
 		vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap=true, silent=true })
 		vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap=true, silent=true })
+		vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd> lua vim.lsp.buf.code_action()<CR>", { noremap=true, silent=true })
 	end
 }
