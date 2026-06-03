96b84201eb3b44d9042fdd6ef128b34d7a01790f
Vinicius Mello <vinicius.mello@isystems.ai>
2026-06-03 09:00:45 -0300

    light theme

--- lua/vinimello/plugins/colorscheme.lua
+++ lua/vinimello/plugins/colorscheme.lua
@@ -4,14 +4,7 @@ return {
 	opts = {
 		transparent_bg=true,
 	},
-	config = function(_, opts)
-		require('dracula').setup(opts)
-		vim.cmd[[colorscheme dracula]]
-
-		-- Força o destaque de referências do LSP a mudar apenas o fundo,
-		-- preservando a sintaxe do Treesitter no texto.
-		vim.api.nvim_set_hl(0, 'LspReferenceText' , { bg = '#44475A', fg = 'NONE', default = false })
-		vim.api.nvim_set_hl(0, 'LspReferenceRead' , { bg = '#44475A', fg = 'NONE', default = false })
-		vim.api.nvim_set_hl(0, 'LspReferenceWrite', { bg = '#44475A', fg = 'NONE', default = false })
+	init = function()
+		vim.cmd[[colorscheme catppuccin-latte]]
 	end
 }
